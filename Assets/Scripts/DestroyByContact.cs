20643e2b69f94b568a419ddfe532265aea7397b4
izayacity <xyang42@ucsc.edu>
2017-11-17 02:20:25 -0800

    Added enemy ship

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -9,15 +9,20 @@ public class DestroyByContact : MonoBehaviour {
 	public int scoreValue;
 
 	private void OnTriggerEnter(Collider other) {
-		if (other.tag == "Boundary")
+
+		if (other.CompareTag("Boundary") || other.CompareTag("Enemy")) { 
 			return;
+		}
+				
+		if(exploision != null) {
+			Instantiate(exploision, transform.position, transform.rotation);
+		}
 
-		Instantiate(exploision, transform.position, transform.rotation);
-
-		if (other.tag == "Player") {
+		if (other.CompareTag("Player")) {
 			Instantiate(playerExploision, other.transform.position, other.transform.rotation);
 			GameController.instance.GameOver();
 		}
+
 		GameController.instance.AddScore(scoreValue);
 		Destroy(other.gameObject);
 		Destroy(gameObject);
