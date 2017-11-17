0bff56fb66fe5c0064cfd94364eac5dcd9c38745
izayacity <xyang42@ucsc.edu>
2017-11-16 19:21:48 -0800

    Added explosion

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject exploision;
+	public GameObject playerExploision;
+
 	private void OnTriggerEnter(Collider other) {
 		if (other.tag == "Boundary")
 			return;
-		
+
+		Instantiate(exploision, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate(playerExploision, other.transform.position, other.transform.rotation);
+		}
+
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 	}
